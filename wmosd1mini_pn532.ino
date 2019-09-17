5b47ed5471dd87c828402931a9121ddefd2e8bcf
maxwiese <maxwiese13@gmail.com>
2019-09-17 07:07:33 +0200

    init

---
+++ wmosd1mini_pn532.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include <PN532_I2C.h>
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_I2C pn532_i2c(Wire);
+NfcAdapter nfc = NfcAdapter(pn532_i2c);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("RinnebühlProgLabs Mifar Card Reader v0.1");
+  nfc.begin();
+
+}
+
+void loop() {
+  Serial.println("\nScan a NFC tag\n");
+
+  if (nfc.tagPresent())
+  {
+    NfcTag tag = nfc.read();
+    Serial.println(tag.getTagType());
+    Serial.print("UID: ");Serial.println(tag.getUidString());
+  }
+  delay(1000);
+}
